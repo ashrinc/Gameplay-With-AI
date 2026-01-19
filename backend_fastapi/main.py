f3381b5a82cad599e4b46f785190d9766b54eb88
ashrinc <160386862+ashrinc@users.noreply.github.com>
2026-01-19 10:49:10 +0530

    Update main.py

--- backend_fastapi/main.py
+++ backend_fastapi/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gameplay-with-ai.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
